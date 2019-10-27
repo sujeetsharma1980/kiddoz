889b72935c537e901ea21c532ca810573b68ee17
Sujeet Sharma <sujeet.sharma@kp.org>
2019-10-26 20:42:52 -0700

    new changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-   
+    apiKey: "AIzaSyAS9yTuuwE9tpc2VLqFUpQlMsAyAg36x18",
+    authDomain: "kiddozz.firebaseapp.com",
+    databaseURL: "https://aviapp-70abf.firebaseio.com",
+    projectId: "aviapp-70abf",
+    storageBucket: "aviapp-70abf.appspot.com",
+    messagingSenderId: "447640335798"
   }
 };
